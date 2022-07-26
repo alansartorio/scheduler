d88951fdc5d72b5ffbb1be4d9636ac7057747be5
Alan Sartorio <alan42ga@hotmail.com>
2022-07-26 06:39:47 -0300

    Moved iterators to separate file.

--- src/main.rs
+++ src/main.rs
@@ -4,75 +4,16 @@
 use std::collections::HashSet;
 use std::fmt::Debug;
 use std::fs::read_to_string;
-use std::path::{PathBuf, Path};
-use std::rc::Rc;
+use std::path::{Path, PathBuf};
 
 use itertools::Itertools;
 //use sqlite;
 
-use scheduler::loader::{load, Code, Subject, SubjectCommision};
-use scheduler::option_generator::generate;
-
-struct Whitelist<'a, I: Iterator<Item = Rc<Subject>>> {
-    iter: I,
-    list: &'a HashSet<Code>,
-}
-impl<'a, I: Iterator<Item = Rc<Subject>>> Whitelist<'a, I> {
-    fn new(iter: I, list: &'a HashSet<Code>) -> Self {
-        Self { iter, list }
-    }
-}
-impl<'a, I: Iterator<Item = Rc<Subject>>> Iterator for Whitelist<'a, I> {
-    type Item = Rc<Subject>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.find(|i| self.list.contains(&i.code))
-    }
-}
-
-struct Blacklist<'a, I: Iterator<Item = Rc<Subject>>> {
-    iter: I,
-    list: &'a HashSet<Code>,
-}
-impl<'a, I: Iterator<Item = Rc<Subject>>> Blacklist<'a, I> {
-    fn new(iter: I, list: &'a HashSet<Code>) -> Self {
-        Self { iter, list }
-    }
-}
-impl<'a, I: Iterator<Item = Rc<Subject>>> Iterator for Blacklist<'a, I> {
-    type Item = Rc<Subject>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.find(|i| !self.list.contains(&i.code))
-    }
-}
-
-trait IterExtensions: Iterator<Item = Rc<Subject>> {
-    fn get_by_code(&mut self, code: Code) -> Option<Rc<Subject>>
-    where
-        Self: Sized,
-    {
-        self.find(|sub| (*sub).code == code)
-    }
-
-    fn whitelist_codes(self, codes: &HashSet<Code>) -> Whitelist<Self>
-    where
-        Self: Sized,
-    {
-        Whitelist::new(self, codes)
-    }
-
-    fn blacklist_codes(self, codes: &HashSet<Code>) -> Blacklist<Self>
-    where
-        Self: Sized,
-    {
-        Blacklist::new(self, codes)
-    }
-}
-
-impl<T: Iterator<Item = Rc<Subject>>> IterExtensions for T {}
-
 use clap::Parser;
+use scheduler::loader::{load, Code, SubjectCommision};
+use scheduler::option_generator::generate;
+mod subject_iter;
+use subject_iter::SubjectIterable;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
---
+++ src/subject_iter.rs
@@ -0,0 +1,65 @@
+use std::{rc::Rc, collections::HashSet};
+
+use scheduler::loader::{Subject, Code};
+
+
+
+pub struct Whitelist<'a, I: Iterator<Item = Rc<Subject>>> {
+    iter: I,
+    list: &'a HashSet<Code>,
+}
+impl<'a, I: Iterator<Item = Rc<Subject>>> Whitelist<'a, I> {
+    pub fn new(iter: I, list: &'a HashSet<Code>) -> Self {
+        Self { iter, list }
+    }
+}
+impl<'a, I: Iterator<Item = Rc<Subject>>> Iterator for Whitelist<'a, I> {
+    type Item = Rc<Subject>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.find(|i| self.list.contains(&i.code))
+    }
+}
+
+pub struct Blacklist<'a, I: Iterator<Item = Rc<Subject>>> {
+    iter: I,
+    list: &'a HashSet<Code>,
+}
+impl<'a, I: Iterator<Item = Rc<Subject>>> Blacklist<'a, I> {
+    pub fn new(iter: I, list: &'a HashSet<Code>) -> Self {
+        Self { iter, list }
+    }
+}
+impl<'a, I: Iterator<Item = Rc<Subject>>> Iterator for Blacklist<'a, I> {
+    type Item = Rc<Subject>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.find(|i| !self.list.contains(&i.code))
+    }
+}
+
+pub trait SubjectIterable: Iterator<Item = Rc<Subject>> {
+    fn get_by_code(&mut self, code: Code) -> Option<Rc<Subject>>
+    where
+        Self: Sized,
+    {
+        self.find(|sub| (*sub).code == code)
+    }
+
+    fn whitelist_codes(self, codes: &HashSet<Code>) -> Whitelist<Self>
+    where
+        Self: Sized,
+    {
+        Whitelist::new(self, codes)
+    }
+
+    fn blacklist_codes(self, codes: &HashSet<Code>) -> Blacklist<Self>
+    where
+        Self: Sized,
+    {
+        Blacklist::new(self, codes)
+    }
+}
+
+impl<T: Iterator<Item = Rc<Subject>>> SubjectIterable for T {}
+
